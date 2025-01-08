dfe37fa01f431228b2b6fd839dfffcb754f90b0e
Anton Caesar <caesaranton700@yahoo.de>
2025-01-08 18:26:55 +0100

    Began implementation of flection training mode

--- src/routes/learn/fi/flection/+page.svelte
+++ src/routes/learn/fi/flection/+page.svelte
@@ -1,1 +1,17 @@
-<p>flection</p>
+<script>
+    import { onMount } from 'svelte';
+    import adjectives from '$lib/learn/fi/adjectives.json';
+  
+    onMount(() => {
+      // Your function to execute on page load
+      console.log("Page has loaded!");
+      showCard();
+    });
+
+    function showCard() {
+    }
+</script>
+
+{@debug adjectives}
+
+{adjectives.vanha.translation}
