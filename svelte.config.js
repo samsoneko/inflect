9fdc2aab34eca33e983a8f5fc0f4d276f914ea1b
Anton Caesar <caesaranton700@yahoo.de>
2025-01-21 23:28:02 +0100

    Prepared config for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,4 +15,8 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+	  adapter: adapter(),
+	}
+  };
